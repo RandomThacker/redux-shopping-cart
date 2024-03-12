8582dd52b790ac1fa5d1628f94c504955b68e351
RandomThacker <randomthacker@gmail.com>
2024-03-12 17:00:07 +0530

    updated payment page

--- frontend/src/components/PaymentCard.jsx
+++ frontend/src/components/PaymentCard.jsx
@@ -4,55 +4,12 @@ import {
   Card,
   CardHeader,
   CardBody,
-  Input,
   Button,
   Typography,
-  Tabs,
-  TabsHeader,
-  TabsBody,
-  Tab,
-  TabPanel,
-  Select,
-  Option,
 } from "@material-tailwind/react";
-import {
-  BanknotesIcon,
-  CreditCardIcon,
-  LockClosedIcon,
-} from "@heroicons/react/24/solid";
-
-function formatCardNumber(value) {
-  const val = value.replace(/\s+/g, "").replace(/[^0-9]/gi, "");
-  const matches = val.match(/\d{4,16}/g);
-  const match = (matches && matches[0]) || "";
-  const parts = [];
-
-  for (let i = 0, len = match.length; i < len; i += 4) {
-    parts.push(match.substring(i, i + 4));
-  }
-
-  if (parts.length) {
-    return parts.join(" ");
-  } else {
-    return value;
-  }
-}
-
-function formatExpires(value) {
-  return value
-    .replace(/[^0-9]/g, "")
-    .replace(/^([2-9])$/g, "0$1")
-    .replace(/^(1{1})([3-9]{1})$/g, "0$1/$2")
-    .replace(/^0{1,}/g, "0")
-    .replace(/^([0-1]{1}[0-9]{1})([0-9]{1,2}).*/g, "$1/$2");
-}
+import { CreditCardIcon, LockClosedIcon } from "@heroicons/react/24/solid";
 
 export default function PaymentCard() {
-  //   const { countries } = useCountries();
-  const [type, setType] = React.useState("card");
-  const [cardNumber, setCardNumber] = React.useState("");
-  const [cardExpires, setCardExpires] = React.useState("");
-
   return (
     <Card className="w-[24rem]">
       <CardHeader
@@ -62,227 +19,43 @@ export default function PaymentCard() {
         className="m-0 grid place-items-center px-4 py-8 text-center w-[24rem]"
       >
         <div className="mb-4 h-20 p-6 text-white">
-          {type === "card" ? (
-            <CreditCardIcon className="h-10 w-10 text-white" />
-          ) : (
-            <img
-              alt="paypal "
-              className="w-14 "
-              src="https://docs.material-tailwind.com/icons/paypall.png"
-            />
-          )}
+          <h1 className="text-2xl font-bold">₹ 2700</h1>
         </div>
-        <Typography variant="h5" color="white">
-          
-        </Typography>
+        <Typography variant="h5" color="white"></Typography>
       </CardHeader>
       <CardBody>
-        <Tabs value={type} className="overflow-visible">
-          <TabsHeader className="relative z-0 ">
-            <Tab value="card" onClick={() => setType("card")}>
-             PhonePay
-            </Tab>
-            <Tab value="paypal" onClick={() => setType("paypal")}>
-             QR Code
-            </Tab>
-          </TabsHeader>
-          <TabsBody
-            className="!overflow-x-hidden !overflow-y-visible "
-            animate={{
-              initial: {
-                x: type === "card" ? 400 : -400,
-              },
-              mount: {
-                x: 0,
-              },
-              unmount: {
-                x: type === "card" ? 400 : -400,
-              },
-            }}
+        <form className="mt-12 flex flex-col gap-4">
+          <div className="my-6 flex flex-col items-center">
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="mb-2 font-bold text-left"
+            >
+              Scan the QR Code to Pay the amount
+            </Typography>
+            <img
+              src="https://www.techopedia.com/wp-content/uploads/2023/03/aee977ce-f946-4451-8b9e-bba278ba5f13.png"
+              style={{ width: "200px" }}
+            />
+          </div>
+          <Typography
+            variant="small"
+            color="blue-gray"
+            className="mb-2 font-bold"
           >
-            <TabPanel value="card" className="p-0">
-              <form className="mt-12 flex flex-col gap-4">
-                <div>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-medium"
-                  >
-                    Your Email
-                  </Typography>
-                  <Input
-                    type="email"
-                    placeholder="name@mail.com"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                </div>
-
-                <div className="my-3">
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-medium "
-                  >
-                    Card Details
-                  </Typography>
-
-                  <Input
-                    maxLength={19}
-                    value={formatCardNumber(cardNumber)}
-                    onChange={(event) => setCardNumber(event.target.value)}
-                    icon={
-                      <CreditCardIcon className="absolute left-0 h-4 w-4 text-blue-gray-300" />
-                    }
-                    placeholder="0000 0000 0000 0000"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                  <div className="my-4 flex items-center gap-4">
-                    <div>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="mb-2 font-medium"
-                      >
-                        Expires
-                      </Typography>
-                      <Input
-                        maxLength={5}
-                        value={formatExpires(cardExpires)}
-                        onChange={(event) => setCardExpires(event.target.value)}
-                        containerProps={{ className: "min-w-[72px]" }}
-                        placeholder="00/00"
-                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                        labelProps={{
-                          className: "before:content-none after:content-none",
-                        }}
-                      />
-                    </div>
-                    <div>
-                      <Typography
-                        variant="small"
-                        color="blue-gray"
-                        className="mb-2 font-medium"
-                      >
-                        CVC
-                      </Typography>
-                      <Input
-                        maxLength={4}
-                        containerProps={{ className: "min-w-[72px]" }}
-                        placeholder="000"
-                        className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                        labelProps={{
-                          className: "before:content-none after:content-none",
-                        }}
-                      />
-                    </div>
-                  </div>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-medium"
-                  >
-                    Holder Name
-                  </Typography>
-                  <Input
-                    placeholder="name@mail.com"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-                </div>
-                <Button size="lg">Pay Now</Button>
-                <Typography
-                  variant="small"
-                  color="gray"
-                  className="mt-2 flex items-center justify-center gap-2 font-medium opacity-60"
-                >
-                  <LockClosedIcon className="-mt-0.5 h-4 w-4" /> Payments are
-                  secure and encrypted
-                </Typography>
-              </form>
-            </TabPanel>
-            <TabPanel value="paypal" className="p-0 w-[24rem]">
-              <form className="mt-12 flex flex-col gap-4">
-                <div>
-                  <Typography
-                    variant="paragraph"
-                    color="blue-gray"
-                    className="mb-4 font-medium"
-                  >
-                    Personal Details
-                  </Typography>
-                  <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-medium "
-                  >
-                    Your Name
-                  </Typography>
-                  <Input
-                    type="name"
-                    placeholder="full name"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900 w-[24rem]"
-                    style={{width:"24rem"}}
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  />
-
-                  {/* <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-medium"
-                  >
-                    Your Email
-                  </Typography>
-                  <Input
-                    type="email"
-                    placeholder="name@mail.com"
-                    className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
-                    labelProps={{
-                      className: "before:content-none after:content-none",
-                    }}
-                  /> */}
-                </div>
-
-                <div className="my-6">
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-bold"
-                  >
-                    Scan the QR Code to Pay the amount
-                  </Typography>
-                 <img src="https://www.techopedia.com/wp-content/uploads/2023/03/aee977ce-f946-4451-8b9e-bba278ba5f13.png" style={{width:"200px"}} />
-                </div>
-                <Typography
-                    variant="small"
-                    color="blue-gray"
-                    className="mb-2 font-bold"
-                  >
-                    Upload the screenshot after payment
-                  </Typography>
-                <input type="file" id="file-input" name="ImageStyle"/>
-                <Button size="lg">pay manually</Button>
-                <Typography
-                  variant="small"
-                  color="gray"
-                  className="flex items-center justify-center gap-2 font-medium opacity-60"
-                >
-                  <LockClosedIcon className="-mt-0.5 h-4 w-4" /> Payments are
-                  secure and encrypted
-                </Typography>
-              </form>
-            </TabPanel>
-          </TabsBody>
-        </Tabs>
+            Upload the screenshot of the payment
+          </Typography>
+          <input type="file" id="file-input" name="ImageStyle" />
+          <Button size="lg">pay</Button>
+          <Typography
+            variant="small"
+            color="gray"
+            className="flex items-center justify-center gap-2 font-medium opacity-60"
+          >
+            <LockClosedIcon className="-mt-0.5 h-4 w-4" /> Payments are secure
+            and encrypted
+          </Typography>
+        </form>
       </CardBody>
     </Card>
   );
